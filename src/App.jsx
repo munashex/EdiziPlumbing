3df01fb6e52729e623672e5cf666a7ed97e50419
munashe <munashemugondaa@gmail.com>
2025-04-16 09:40:29 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./Components/Navbar"
+
+const App = () => {
+
+  return (
+    <div className="px-4 md:px-8 lg:px-16 text-[#26292F]">
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,14 @@
+import Logo from '../images/Logo.png' 
+
+
+const Navbar = () => {
+
+    return (
+        <div>
+         <h1>Navbar component</h1> 
+         <img src={Logo}/>
+        </div>
+    )
+}
+
+export default Navbar 
